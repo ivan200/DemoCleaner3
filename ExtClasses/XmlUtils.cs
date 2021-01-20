7e8382ee0ed6a83c3b272eed21f035f50009d655
Vít Šesták <you@example.com>
2021-01-20 21:46:07 +0100

    Fix invalid XML attribute names

--- ExtClasses/XmlUtils.cs
+++ ExtClasses/XmlUtils.cs
@@ -1,14 +1,35 @@
-﻿using System.Xml;
+﻿using System;
+using System.Xml;
 using System.Collections.Generic;
 
 namespace DemoCleaner3 {
     public static class XmlUtils {
+        private static String NormalizeAttributeName(String name) {
+            String output = "";
+            if (name.Length == 0 || (name[0] >= '0' && name[0] <= '9') )
+            {
+                output += "_";
+            }
+            foreach (var c in name)
+            {
+                if ((c >= '0' && c <= '9') || (c >= 'a' && c <= 'z') ||(c >= 'A' && c <= 'Z') || (c == '-'))
+                {
+                    output += c;
+                }
+                else
+                {
+                    output += "_";
+                }
+            }
+            return output;
+        }
+
         public static XmlNode ExportXml(XmlDocument doc, Dictionary<string, Dictionary<string, string>> map) {
             var node = doc.CreateNode("element", "demoFile", "");
             foreach (var item in map) {
                 var el = doc.CreateNode("element", item.Key, "");
                 foreach (var subItem in item.Value) {
-                    var attr = doc.CreateAttribute(subItem.Key.Replace(' ', '-'));
+                    var attr = doc.CreateAttribute(NormalizeAttributeName(subItem.Key));
                     attr.Value = subItem.Value;
                     el.Attributes.Append(attr);
                 }
