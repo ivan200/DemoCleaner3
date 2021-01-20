c98130584409a609d13929bc307d86b0f6d43053
Ivan <79067180651@ya.ru>
2021-01-20 22:50:43 +0300

    little refactor xml output

---
+++ ExtClasses/XmlUtils.cs
@@ -0,0 +1,27 @@
+﻿using System.Xml;
+using System.Collections.Generic;
+
+namespace DemoCleaner3 {
+    public static class XmlUtils {
+        public static XmlNode ExportXml(XmlDocument doc, Dictionary<string, Dictionary<string, string>> map) {
+            var node = doc.CreateNode("element", "demoFile", "");
+            foreach (var item in map) {
+                var el = doc.CreateNode("element", item.Key, "");
+                foreach (var subItem in item.Value) {
+                    var attr = doc.CreateAttribute(subItem.Key.Replace(' ', '-'));
+                    attr.Value = subItem.Value;
+                    el.Attributes.Append(attr);
+                }
+                node.AppendChild(el);
+            }
+            return node;
+        }
+
+        public static string FriendlyInfoToXmlString(Dictionary<string, Dictionary<string, string>> friendlyInfo) {
+            XmlDocument doc = new XmlDocument();
+            return ExportXml(doc, friendlyInfo).OuterXml;
+        }
+
+    }
+
+}
--- Program.cs
+++ Program.cs
@@ -1,40 +1,19 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.IO;
 using System.Reflection;
 using System.Windows.Forms;
-using System.Xml;
 
 namespace DemoCleaner3
 {
     static class Program
     {
-
-        static XmlNode ExportXml(XmlDocument doc, Dictionary<String, System.Collections.Generic.Dictionary<string, string>> map)
-        {
-            var node = doc.CreateNode("element", "demoFile", "");
-            foreach(var item in map)
-            {
-                var el = doc.CreateNode("element", item.Key, "");
-                foreach(var subItem in item.Value)
-                {
-                    var attr = doc.CreateAttribute(subItem.Key.Replace(' ', '-'));
-                    attr.Value = subItem.Value;
-                    el.Attributes.Append(attr);
-                }
-                node.AppendChild(el);
-            }
-            return node;
-        }
-
         /// <summary>
-        /// Главная точка входа для приложения.
+        /// The main entry point for the app.
         /// </summary>
         [STAThread]
-        static void Main(String[] argg)
+        static void Main(String[] argg) 
         {
-            AppDomain.CurrentDomain.AssemblyResolve += (sender, arg) => 
-            {
+            AppDomain.CurrentDomain.AssemblyResolve += (sender, arg) => {
                 if (arg.Name.StartsWith("LinqBridge")) return Assembly.Load(Properties.Resources.LinqBridge);
                 return null;
             };
@@ -46,7 +25,7 @@ namespace DemoCleaner3
             bool xmlOutput = false;
             if (argg.Length == 1) {
                 demoFile = new FileInfo(argg[0]);
-            }else if ((argg.Length == 2) && (argg[0] == "--xml")) {
+            } else if ((argg.Length == 2) && (argg[0] == "--xml")) {
                 demoFile = new FileInfo(argg[1]);
                 xmlOutput = true;
             }
@@ -55,8 +34,8 @@ namespace DemoCleaner3
                 demoInfoForm.demoFile = demoFile;
                 if (xmlOutput) {
                     Demo demo = Demo.GetDemoFromFileRaw(demoFile);
-                    XmlDocument doc = new XmlDocument();
-                    Console.WriteLine(ExportXml(doc, demo.rawInfo.getFriendlyInfo()).OuterXml);
+                    var xmlString = XmlUtils.FriendlyInfoToXmlString(demo.rawInfo.getFriendlyInfo());
+                    Console.WriteLine(xmlString);
                 } else {
                     Application.Run(demoInfoForm);
                 }
